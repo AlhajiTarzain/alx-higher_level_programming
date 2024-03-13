3292363fc145cea5a982ea7c4200ed82b21590d5
AlhajiTarzain <dredeen21@gmail.com>
2024-03-13 14:26:10 -0700

    o

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table first_table from the database hbtn_0c_0 in  server.
+-- All fields  be printed
+-- The database name be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
